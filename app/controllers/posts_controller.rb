c74dd3d59b6fc4cc43138ed44a99b5797e818186
katayama-Web <kairikatayama@gmail.com>
2022-10-03 08:43:01 +0000

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to ''
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :post_image, :camara, :lens, :iso, :f_number, :shutter_speed, :remark)
   end
 end
